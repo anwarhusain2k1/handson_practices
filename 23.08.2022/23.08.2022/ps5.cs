35a374f7aef6172a69a1429dc767c49696ebdde3
anwarhusain2k1 <anwar2k1@gmail.com>
2022-08-24 09:53:03 +0530

    23.08

---
+++ 23.08.2022/23.08.2022/ps5.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._08._2022
+{
+    internal class ps5
+    {
+        static void Main(string[] args)
+        {
+            Employee[] emp = new Employee[4];
+            
+            for (int i = 0; i < 4; i++)
+            {
+
+                Console.WriteLine("enter employee number");
+                int empno = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter employee name");
+                string name = Console.ReadLine();
+                Console.WriteLine("enter salary");
+                int salary = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter department number");
+                int depno = Convert.ToInt32(Console.ReadLine());
+                emp[i] = new Employee(empno, name, salary, depno);
+                
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                emp[i].display();
+            }
+        }
+        struct Employee
+        {
+            public int empno;
+            public string name;
+            public int salary;
+            public int depno;
+
+            public Employee(int empno, string name, int sal, int dno)
+            {
+                this.empno = empno;
+                this.name = name;
+                this.salary = sal;
+                this.depno = dno;
+            }
+
+            public void display()
+            {
+                Console.WriteLine("employee id:" + empno);
+                Console.WriteLine("employee name :" + name);
+                Console.WriteLine("employee salary :" + salary);
+                Console.WriteLine("dept number:" + depno);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ 23.08.2022/23.08.2022/ps7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._08._2022
+{
+    internal class ps7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int c = a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadLine();
+            }
+        }
+    }
+}
