9c6cd1ef4aedd6e2701c473828dbbc67f036ad7b
anwarhusain2k1 <anwar2k1@gmail.com>
2022-08-29 21:03:29 +0530

    29.08

---
+++ 29.08.2022/29.08.2022/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29._08._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("userdefined stack :\n");
+            UserDataStore<string> stringdata = new UserDataStore<string>();
+            stringdata.AddOrEdit(0, "Hello");
+            string printdata = stringdata.ReturnData(0);
+            Console.WriteLine(printdata);
+            Console.WriteLine("-------------------");
+            stringdata.AddOrEdit(0, "Welcome");
+            stringdata.ShowData(0);
+            Console.WriteLine("======================");
+            Console.WriteLine("userdefined singly linkedlist:\n");
+            LinkedList sllist = new LinkedList();
+            sllist.PrintAllNodes();
+            sllist.AddAtLast(5);
+            sllist.AddAtLast(6);
+            sllist.AddAtLast("seven");
+            sllist.AddAtLast(8);
+            sllist.PrintAllNodes();
+            Console.WriteLine();
+            sllist.AddAtStart(4);
+            sllist.PrintAllNodes();
+            Console.WriteLine();
+
+            sllist.RemoveFromStart();
+            sllist.PrintAllNodes();
+            Console.WriteLine("======================");
+            Console.WriteLine("custom queue:\n");
+            LinearQueue Q = new LinearQueue(5);
+
+            Q.insert(10);
+            Q.insert(20);
+            Q.insert(30);
+            Q.insert(40);
+            Q.insert(50);
+
+            Console.WriteLine("Items are : ");
+            Q.printQueue();
+            Q.delete();
+            Q.delete();
+            Console.WriteLine("Items are : ");
+            Q.printQueue();
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ 29.08.2022/29.08.2022/singlylinkedlist.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29._08._2022
+{
+    public class Node
+    {
+        public Node Next;
+        public object Value;
+    }
+
+    public class LinkedList
+    {
+        private Node head;
+        private Node current;//This will have latest node
+        public int Count;
+
+
+        public LinkedList()
+        {
+            head = new Node();
+            current = head;
+        }
+
+        public void AddAtLast(object data)
+        {
+            Node newNode = new Node();
+            newNode.Value = data;
+            current.Next = newNode;
+            current = newNode;
+            Count++;
+        }
+
+        public void AddAtStart(object data)
+        {
+            Node newNode = new Node() { Value = data };
+            newNode.Next = head.Next;//new node will have reference of head's next reference
+            head.Next = newNode;//and now head will refer to new node
+            Count++;
+        }
+
+        public void RemoveFromStart()
+        {
+            if (Count > 0)
+            {
+                head.Next = head.Next.Next;
+                Count--;
+            }
+            else
+            {
+                Console.WriteLine("No element exist in this linked list.");
+            }
+        }
+
+        public void PrintAllNodes()
+        {
+            //Traverse from head
+            Node curr = head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Value);
+                Console.Write("  ");
+            }
+
+        }
+    }
+
+    
+}
